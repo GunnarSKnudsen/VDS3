0cee7631a822af07897c3f9f7735d399f193882d
GunnarSKnudsen <waxfactor2nd@gmail.com>
2022-01-14 18:30:10 +0100

    3dplot kinda working

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,8 @@ from dash.dependencies import Input, Output# Load Data
 
 import json
 
+import numpy as np
+
 import dash
 import dash_table as dt
 from dash import html
@@ -18,13 +20,13 @@ import pandas as pd
 
 
 from sklearn.metrics import mean_absolute_percentage_error
-
+import plotly.figure_factory as ff
 
 
 # figure out the index thing
 #df = pd.read_csv('dataframe_to_visualize.csv', index_col=0, parse_dates=True)
 df = pd.read_csv('dataframe_to_visualize_subset.csv', index_col=0, parse_dates=True)
-df = pd.read_csv('dataframe_to_visualize.csv', index_col=0, parse_dates=True)
+#df = pd.read_csv('dataframe_to_visualize.csv', index_col=0, parse_dates=True)
 print(f"Read in dataframe with shape {df.shape}")
 
 # Add final data processing
@@ -430,83 +432,247 @@ app.layout = html.Div(
         ]
 
 )
-#Do this https://dash.plotly.com/dash-core-components/rangeslider
 
-# Callback for timeseries price
-#@app.callback(Output('timeseries', 'figure'),
-#              [Input('slider2', 'value')])
-#def update_graph2(val):
-#	print(val)
-
-
+# Hover callback:
+# When hovering on the scatterplot, current point is described in table below.
 @app.callback(
-    #Output('hover-data', 'children'),
     Output("hover-data", "data"),
     Input('timeseries', 'hoverData'))
 def display_hover_data(hoverData):
+	# Only act if point is hovered
 	if hoverData is not None:
-		#print("Did a callback")
+		# Find which points are hovered by looping through all (1)
 		indices = []
 		for p in hoverData['points']:
 			custIndex = p['customdata'][0]
 			indices.append(custIndex)
 
 		df_selected = df[df.dummyKey.isin(indices)]
-		#print(df_selected)
 
-		#return json.dumps(hoverData, indent=2)
 		return df_selected.to_dict('records')
 
 
-
+# Select callback:
+# When selecting points on the scatterplot, (brushing), I'm linking to predictions plot
+## Also showing table of selected data
 @app.callback(
-	#Output('selected-data', 'children'),
 	[Output("selected-data", "data"),
 	 Output('modelPredictionsGraph', 'figure'),
-	 Output('mapeDiv', 'children')
-	],
+	 Output('mapeDiv', 'children')],
 	Input('timeseries', 'selectedData'))
 def display_selected_data(selectedData):
+	#Only return if anything is selected
 	if selectedData is not None:
-		#print("Did a callback")
+		# Get keys of all selected points
 		indices = []
 		for p in selectedData['points']:
 			custIndex = p['customdata'][0]
 			indices.append(custIndex)
 
+		# Filter for selected points
 		df_selected = df[df.dummyKey.isin(indices)]
-		#print(df_selected)
 
+		print(df_selected.shape)
+
+		# Calculate MAPE for selected points
 		mape = mean_absolute_percentage_error(df_selected.BodyweightKg, df_selected.predicted)
 		mapeString = "MAPE= {:.0%}".format(mape)
 
+		df_corr = df_selected.corr()
+		print(df_corr)
 
-	figure = px.scatter(
-		df_selected
-		, x="BodyweightKg"
-		, y=["predicted"]
-		#, color="Continent"
-		, custom_data = ['dummyKey']
-		, trendline="ols"
-		, title="Model - Weight vs predicted"
-	)
+		mask = np.triu(np.ones_like(df_corr, dtype=bool))
+		df_mask = df_corr.mask(mask)
 
-	figure.update_layout({
-		'plot_bgcolor': '#161616',
-		'paper_bgcolor': '#161616',
-		'template':'plotly_dark'
-	})
+		figure = ff.create_annotated_heatmap(z=df_mask.to_numpy(), 
+                                  x=df_mask.columns.tolist(),
+                                  y=df_mask.columns.tolist(),
+                                  colorscale=px.colors.diverging.RdBu,
+                                  hoverinfo="none", #Shows hoverinfo for null values
+                                  showscale=True, ygap=1, xgap=1
+                                 )
 
-	figure.update_layout(
-		dragmode='select',
-	)
+		figure.update_xaxes(side="bottom")
 
-	return df_selected.to_dict('records'), figure, mapeString
+		figure.update_layout(
+		    title_text='Heatmap', 
+		    title_x=0.5, 
+		    width=1000, 
+		    height=1000,
+		    xaxis_showgrid=False,
+		    yaxis_showgrid=False,
+		    xaxis_zeroline=False,
+		    yaxis_zeroline=False,
+		    yaxis_autorange='reversed',
+		    template='plotly_white'
+		)
 
 
 
 
+		# (Re)generate the plot
+		#figure = px.scatter(
+		#	df_selected
+		#	, x="BodyweightKg"
+		#	, y=["predicted"]
+		#	, color="Sex"
+		#	, custom_data = ['dummyKey']
+		#	, trendline="ols"
+		#	, title="Model - Weight vs predicted"
+		#)
 
+		figure = go.Heatmap(
+			z=df_corr.values,#to_numpy(),
+			x=df_corr.index.values,#columns.tolist(),
+			y=df_corr.columns.values)#index.tolist(),
+			#zmax=1, zmin=-1,
+			#showscale=True,
+			#hoverongaps=True
+		#)
+
+		fig = ff.create_annotated_heatmap(z=df_corr.to_numpy(), 
+										x=df_corr.columns.tolist(),
+										y=df_corr.columns.tolist(),
+										colorscale=px.colors.diverging.RdBu,
+										hoverinfo="none", #Shows hoverinfo for null values
+										showscale=True, ygap=1, xgap=1
+										)
+
+		fig.update_xaxes(side="bottom")
+
+		fig.update_layout(
+			title_text='Heatmap', 
+			title_x=0.5, 
+			#width=1000, 
+			#height=1000,
+			xaxis_showgrid=False,
+			yaxis_showgrid=False,
+			xaxis_zeroline=False,
+			yaxis_zeroline=False,
+			yaxis_autorange='reversed',
+			template='plotly_white'
+		)
+
+# NaN values are not handled automatically and are displayed in the figure
+# So we need to get rid of the text manually
+		for i in range(len(fig.layout.annotations)):
+			if fig.layout.annotations[i].text == 'nan':
+				fig.layout.annotations[i].text = ""
+
+
+		fig = px.scatter_3d(df_selected, x='BodyweightKg', y='Mean height', z='TotalKg',
+				color='Sex', symbol='Sex')
+
+
+		# Update layout
+		fig.update_layout({
+			'plot_bgcolor': '#161616',
+			'paper_bgcolor': '#161616',
+			'template':'plotly_dark'
+		})
+
+		#draw a square
+		x = [0, 1, 0, 1, 0, 1, 0, 1]
+		y = [0, 1, 1, 0, 0, 1, 1, 0]
+		z = [0, 0, 0, 0, 1, 1, 1, 1]
+
+		x = df_selected['Mean height'].values
+		y1 = df_selected['BodyweightKg'].values
+		y2 = df_selected['predicted'].values
+		z = df_selected['TotalKg'].values
+		print(y1)
+
+		#the start and end point for each line
+		pairs = [(0,6), (1,7)]
+
+		trace1 = go.Scatter3d(
+			x=x,
+			y=y1,
+			z=z,
+			mode='markers',
+			marker=dict(color="blue"),
+			name='BW (True)'#, 			color="red"
+		)
+		
+
+		x_lines = list()
+		y_lines = list()
+		z_lines = list()
+
+		for i in range(len(x)):
+			x_lines.append(x[i])
+			x_lines.append(x[i])
+			y_lines.append(y1[i])
+			y_lines.append(y2[i])
+			z_lines.append(z[i])
+			z_lines.append(z[i])
+			x_lines.append(None)
+			y_lines.append(None)
+			z_lines.append(None)
+
+		trace2 = go.Scatter3d(
+			x=x,
+			y=y2,
+			z=z,
+			mode='markers',
+			marker=dict(color="red"),
+			name='BW (Pred)'#, 			color="red"
+		)
+
+		trace3 = go.Scatter3d(
+			x=x_lines,
+			y=y_lines,
+			z=z_lines,
+			mode='lines',
+			name=""
+		)
+
+		fig = go.Figure(data=[trace1, trace2, trace3])
+
+		fig.update_layout(legend=dict(
+		orientation="h",
+		yanchor="bottom",
+		y=1.02,
+		xanchor="right",
+		x=1
+		))
+
+		fig.update_layout(scene=dict(xaxis=dict(title_text='Mean height (cm)'),
+									yaxis=dict(title_text='Bodywieght (kg)'),
+									zaxis=dict(title_text='Total (kg)'),
+			))
+
+	
+
+		print("I did create something")
+
+				# Update layout
+		fig.update_layout({
+			'plot_bgcolor': '#161616',
+			'paper_bgcolor': '#161616',
+			'template':'plotly_dark'
+		})
+
+		#https://stackoverflow.com/questions/42301481/adding-specific-lines-to-a-plotly-scatter3d-plot
+
+		# Default option
+		#figure.update_layout(
+			#dragmode='select',
+		#)
+
+		# Ensure a square plot
+		#axis_min = min(df_selected.BodyweightKg.min(), df_selected.predicted.min()) * 0.95
+		#axis_max = min(df_selected.BodyweightKg.max(), df_selected.predicted.max()) * 1.05
+		#figure.update_xaxes(range=[axis_min, axis_max])
+		#figure.update_yaxes(range=[axis_min, axis_max])
+
+		# Over and out
+		return df_selected.to_dict('records'), fig, mapeString
+
+
+# "Main" callback
+# Update whenever filters are modified
+## Outputs three plots, as well as optionally drill down of filters
 @app.callback([Output('timeseries', 'figure')
 			,  Output('timeseries2', 'figure')
 			,  Output('fig3Div', 'figure')
@@ -517,8 +683,6 @@ def display_selected_data(selectedData):
 			,  Output('parentFederationSelector', 'options')
 			,  Output('validResSelector', 'options')
 			,  Output('sexSelector', 'options')
-			#,  Output('WeightSliderRange', 'children')
-			#,  Output('HeightSliderRange', 'children')
 			,  Output('rowFoundDiv', 'children')
 			],[Input('drillDownFilterSelector', 'value')
 			,  Input('nSamplesTotal', 'value')
@@ -591,66 +755,56 @@ def update_output(drillDownFilterSelector
 	if sexSelector is None:
 		sexSelector = []
 
-
-	# Update the slider value
+	# Update the slider values
+	## These are no longer printed, but used to be under the sliders
 	WeightSliderRangeValue = str(slider2)
 	HeightSliderRangeValue = str(slider3)
 
+	# Clone dataframe for refiltering
 	df_filtered = df.copy()	
 
-	print(f"This is the input: {continentSelector}")
+	# Filter dataset based on inputs
 	if len(continentSelector) > 0:
 		df_filtered = df_filtered[df_filtered.Continent.isin(continentSelector)]
-	print(f"This is the input: {countrySelector}")
 	if len(countrySelector) > 0:
 		df_filtered = df_filtered[df_filtered.Country.isin(countrySelector)]
-	print(f"This is the input: {eventSelector}")
 	if len(eventSelector) > 0:
 		df_filtered = df_filtered[df_filtered.Event.isin(eventSelector)]
-	print(f"This is the input: {eventSelector}")
 	if len(equipmentSelector) > 0:
 		df_filtered = df_filtered[df_filtered.Equipment.isin(equipmentSelector)]
-	print(f"This is the input: {parentFederationSelector}")
 	if len(parentFederationSelector) > 0:
 		df_filtered = df_filtered[df_filtered.ParentFederation.isin(parentFederationSelector)]
-	print(f"This is the input: {validResSelector}")
-	if len(validResSelector) > 0:
+	if len(sexSelector) > 0:
+		df_filtered = df_filtered[df_filtered.Sex.isin(sexSelector)]
+	if len(validResSelector) > 0: # Not proud of this one, but it works.. Should be switched from dropdown
 		filtering_list = []
+		# Create boolean list from strings
+		## Weird hack, as I would otherwise return boolean to the dropdown, which breaks a lot
 		for item in validResSelector:
 			if item == 'True':
 				filtering_list.append(True)
 			if item == 'False':
 				filtering_list.append(False)
 		df_filtered = df_filtered[df_filtered.Valid_Results.isin(filtering_list)]
-	print(f"This is the input: {sexSelector}")
-	if len(sexSelector) > 0:
-		df_filtered = df_filtered[df_filtered.Sex.isin(sexSelector)]
-
-
 	
-	# Ranges
+	# Filter based on slider ranges
 	df_filtered = df_filtered[df_filtered.BodyweightKg.fillna(0)>= slider2[0]]	
 	df_filtered = df_filtered[df_filtered.BodyweightKg.fillna(0)<= slider2[1]]	
 
 	df_filtered = df_filtered[df_filtered['Mean height'].fillna(0)>= slider3[0]]	
 	df_filtered = df_filtered[df_filtered['Mean height'].fillna(0)<= slider3[1]]	
-	#print(slider2[0], slider2[1])
 
-	#print(df_filtered)
-
+	# Count how many rows have been found
 	rowsFound = df_filtered.shape[0]
 
-	# Filter the dataset:
-	## And subselect because of performance
+	# Do a subselect after filtering.
 	if nSamplesTotal > df_filtered.shape[0]:
 		nSamplesTotal = df_filtered.shape[0]
 	df_filtered = df_filtered.sample(nSamplesTotal, replace=False)
 	rowsShown = df_filtered.shape[0]
 
-	print(df_filtered.Valid_Results)
-
-
 	# Refilter filters
+	## If drilldown is enabled, we need to find new values to select
 	if drillDownFilterSelector == 'No':
 		continent_options = continent_options_all
 		country_options = country_options_all
@@ -669,26 +823,32 @@ def update_output(drillDownFilterSelector
 		valid_res_options = get_options(df_filtered['Valid_Results'].unique())
 		sex_options = get_options(df_filtered['Sex'].unique())
 
-	# Do some plotting
-	print(continentSelector)
+	# Create the plots	
 
-	
+	## Standard scatterplot
 	figure = px.scatter(
         df_filtered
         , x="BodyweightKg"
         , y="TotalKg"
         , color="Continent"
+        # Append key for filtering
         , custom_data = ['dummyKey']
-
         , title="Continent"
     )
 
-	print(df_filtered.shape)
+	## Update layout
+	figure.update_layout({
+		'plot_bgcolor': '#161616',
+		'paper_bgcolor': '#161616',
+		'template':'plotly_dark'
+	})
 
+    ## Default action
+	figure.update_layout(
+		dragmode='select',
+	)
 
-	print("HERE!")
-	print(df_filtered.shape)
-	# Do subsampling here!!!
+	# KDE Plot - based on selectors
 	figure3 = px.density_contour(
         df_filtered
         , x=xAxisSelectorKDE
@@ -699,64 +859,50 @@ def update_output(drillDownFilterSelector
 		, marginal_y="histogram"
     )
 
+	## Update layout
+	figure3.update_layout({
+		'plot_bgcolor': '#161616',
+		'paper_bgcolor': '#161616',
+		'template':'plotly_dark'
+	})
+    ## Default action
+	figure3.update_layout(
+		dragmode='select',
+	)
+
+	# Do aggregation
 	df_grouped = df_filtered.groupby(['CompetitionYear'], as_index = False)[['Best3SquatKg', 'Best3BenchKg', 'Best3DeadliftKg']].mean()
-	#print(df_grouped)
+	
+	# Show timeline
 	figure2 = px.area(
         df_grouped
         , x="CompetitionYear"
         , y=["Best3SquatKg", "Best3BenchKg", "Best3DeadliftKg"]
-        #, color="Continent"
-        #, color_continuous_scale=colorscale
-        #, render_mode="webgl"
         , title="Average score over time"
 	)
 	
-	figure.update_layout({
-		'plot_bgcolor': '#161616',
-		'paper_bgcolor': '#161616',
-		'template':'plotly_dark'
-		})
-
-	figure.update_layout(
-		dragmode='select',
-	)
-
+	## Update layout
 	figure2.update_layout({
 		'plot_bgcolor': '#161616',
 		'paper_bgcolor': '#161616',
 		'template':'plotly_dark'
 		})
+	## Default action
 	figure2.update_layout(
 		dragmode='select',
 	)
 
-	figure3.update_layout({
-		'plot_bgcolor': '#161616',
-		'paper_bgcolor': '#161616',
-		'template':'plotly_dark'
-		})
-	figure3.update_layout(
-		dragmode='select',
-	)
-	
-
-	print(df.shape)
-	print(df_filtered.shape)
-
-
+	# Generate counter text in the filters
 	if rowsFound > nSamplesTotal:
 		counterText = 'Found ' + str(rowsFound) + ' competitors matching filter. Showing ' + str(rowsShown) + ' competitors'
 	else:
 		counterText = 'Found ' + str(rowsFound) + ' competitors matching filter.'
-	print(counterText)
-
+	
+	# Return everything
 	return figure, figure2, figure3\
 	, continent_options, country_options, event_options, equipment_options, parent_federation_options, valid_res_options, sex_options\
 	, counterText
 	#, WeightSliderRangeValue, HeightSliderRangeValue
 	
-
-
-
 if __name__ == '__main__':
     app.run_server(debug=True)
